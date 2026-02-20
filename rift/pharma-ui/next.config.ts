ccbc6fcef0c61641488c7715f4304e9e4e20abed
deshpandepralhad <adeshpande948@gmail.com>
2026-02-20 07:57:33 +0530

    fix turbopack build error

--- rift/pharma-ui/next.config.ts
+++ rift/pharma-ui/next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Remove turbopack config entirely - don't set it to null
-  // This tells Next.js to use the default bundler (webpack)
-  
-  // If you need webpack customizations, they go here
-  webpack: (config, { isServer }) => {
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
